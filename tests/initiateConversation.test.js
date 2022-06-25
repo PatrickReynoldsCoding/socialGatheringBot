fd7485c837464cc343510abd8824a954694ef45c
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-06-25 16:03:18 +0100

    tests complete for initiateConversation

--- tests/initiateConversation.test.js
+++ tests/initiateConversation.test.js
@@ -1,16 +1,35 @@
-const initiateConversation = require('../lib/initiateCoverstation')
+const Conversation = require('../lib/coverstation')
+const initiateConversation = require('../lib/coverstation')
 
 describe('initiateConversations', () => {
   it('returns a neutral/polite responce', () => {
-    expect(initiateConversation(1)).toBe("Hey, how are you?")
+    const conversation = new Conversation;
+
+    expect(conversation.initiateConversation(1)).toBe("Hey, how are you?")
   })
   it('returns a positive/happy responce', () => {
-    expect(initiateConversation(2)).toBe("omg, it's so good to see you!")
+    const conversation = new Conversation;
+
+    expect(conversation.initiateConversation(2)).toBe("omg, it's so good to see you!")
   })
   it('returns a positive/happy responce', () => {
-    expect(initiateConversation(3)).toBe("No way!!! come here! how you doing mate!!!")
+    const conversation = new Conversation;
+
+    expect(conversation.initiateConversation(3)).toBe("No way!!! come here! how you doing mate!!!")
   })
   it('returns a positive/happy responce', () => {
-    expect(initiateConversation(4)).toBe("Hi.")
+    const conversation = new Conversation;
+
+    expect(conversation.initiateConversation(4)).toBe("Hi.")
+  })
+  it('returns a positive/happy responce', () => {
+    const conversation = new Conversation;
+    
+    expect(conversation.tone).toBe(null)
+  })
+  it('returns a positive/happy responce', () => {
+    const conversation = new Conversation;
+    
+    expect(conversation.tone).toBe(null)
   })
 })
