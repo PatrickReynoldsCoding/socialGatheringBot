f08dae16f89ec05769d19fc5877a2a7c965cab73
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-06-25 16:47:11 +0100

    edge cases tested for editTone

--- tests/initiateConversation.test.js
+++ tests/initiateConversation.test.js
@@ -35,19 +35,33 @@ describe("initiateConversations", () => {
   it("returns goodNews for tone", () => {
     const conversation = new Conversation();
 
-    conversation.editTone(1);
+    conversation.verifyTone(1);
     expect(conversation.tone).toBe("goodNews");
   });
   it("returns neutral for tone", () => {
     const conversation = new Conversation();
 
-    conversation.editTone(2);
+    conversation.verifyTone(2);
     expect(conversation.tone).toBe("neutral");
   });
   it("returns badNews for tone", () => {
     const conversation = new Conversation();
 
-    conversation.editTone(3);
+    conversation.verifyTone(3);
     expect(conversation.tone).toBe("badNews");
   });
+  it("returns error for tone", () => {
+    const conversation = new Conversation();
+
+    
+    expect(conversation.verifyTone(4)).toBe("Pick again. There are only 3 options:\n1 = good news\n2 = neutral\n3 = bad news");
+  });
+  it("returns error for tone", () => {
+    const conversation = new Conversation();
+
+    conversation.verifyTone("one");
+    expect(conversation.verifyTone("one")).toBe("Pick again. There are only 3 options:\n1 = good news\n2 = neutral\n3 = bad news");
+  });
+
+  //TEST Good news responce
 });
