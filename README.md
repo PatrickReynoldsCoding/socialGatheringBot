e9cf6a2f230cbee9577a5b5fdfd4bd8988958dd1
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-06-25 16:23:08 +0100

    editTone tested

--- README.md
+++ README.md
@@ -1,17 +1,11 @@
-DESIGN 
-------
+## DESIGN
 
 output table
 
+1.  expect(initiateConversation.one).toBe("Hey, how are you?")
 
-1.
-expect(initiateConversation.one).toBe("Hey, how are you?")
+2.  expect(initiateConversation.two).toBe("omg, it's so good to see you!")
 
-2.
-expect(initiateConversation.two).toBe("omg, it's so good to see you!")
+3.  expect(initiateConversation.three).toBe("No way!!! come here! how you doing mate!!!")
 
-3.
-expect(initiateConversation.three).toBe("No way!!! come here! how you doing mate!!!")
-
-4.
-expect(initiateConversation.four).toBe("Hi.")
+4.  expect(initiateConversation.four).toBe("Hi.")
--- tests/initiateConversation.test.js
+++ tests/initiateConversation.test.js
@@ -1,35 +1,53 @@
-const Conversation = require('../lib/coverstation')
-const initiateConversation = require('../lib/coverstation')
+const Conversation = require("../lib/coverstation");
+const initiateConversation = require("../lib/coverstation");
 
-describe('initiateConversations', () => {
-  it('returns a neutral/polite responce', () => {
-    const conversation = new Conversation;
+describe("initiateConversations", () => {
+  it("returns a neutral/polite responce", () => {
+    const conversation = new Conversation();
 
-    expect(conversation.initiateConversation(1)).toBe("Hey, how are you?")
-  })
-  it('returns a positive/happy responce', () => {
-    const conversation = new Conversation;
+    expect(conversation.initiateConversation(1)).toBe("Hey, how are you?");
+  });
+  it("returns a positive/happy responce", () => {
+    const conversation = new Conversation();
 
-    expect(conversation.initiateConversation(2)).toBe("omg, it's so good to see you!")
-  })
-  it('returns a positive/happy responce', () => {
-    const conversation = new Conversation;
+    expect(conversation.initiateConversation(2)).toBe(
+      "omg, it's so good to see you!"
+    );
+  });
+  it("returns a very positive/enthusiastic/excited responce", () => {
+    const conversation = new Conversation();
 
-    expect(conversation.initiateConversation(3)).toBe("No way!!! come here! how you doing mate!!!")
-  })
-  it('returns a positive/happy responce', () => {
-    const conversation = new Conversation;
+    expect(conversation.initiateConversation(3)).toBe(
+      "No way!!! come here! how you doing mate!!!"
+    );
+  });
+  it("returns a unengaging/dismissive/attempt to end interaction responce", () => {
+    const conversation = new Conversation();
 
-    expect(conversation.initiateConversation(4)).toBe("Hi.")
-  })
-  it('returns a positive/happy responce', () => {
-    const conversation = new Conversation;
-    
-    expect(conversation.tone).toBe(null)
-  })
-  it('returns a positive/happy responce', () => {
-    const conversation = new Conversation;
-    
-    expect(conversation.tone).toBe(null)
-  })
-})
+    expect(conversation.initiateConversation(4)).toBe("Hi.");
+  });
+  it("returns null for tone", () => {
+    const conversation = new Conversation();
+
+    expect(conversation.tone).toBe(null);
+  });
+
+  it("returns goodNews for tone", () => {
+    const conversation = new Conversation();
+
+    conversation.editTone(1);
+    expect(conversation.tone).toBe("goodNews");
+  });
+  it("returns neutral for tone", () => {
+    const conversation = new Conversation();
+
+    conversation.editTone(2);
+    expect(conversation.tone).toBe("neutral");
+  });
+  it("returns badNews for tone", () => {
+    const conversation = new Conversation();
+
+    conversation.editTone(3);
+    expect(conversation.tone).toBe("badNews");
+  });
+});
